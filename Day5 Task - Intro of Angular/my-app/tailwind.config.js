83b8ed00a97085626b8b2f31f67e9bf96fb397cc
parangi-rathod <parangi.rathod@bacancy.com>
2024-03-25 17:33:13 +0530

    Angular day 5

---
+++ Day5 Task - Intro of Angular/my-app/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        orange: '#E36040',
+        yellow: '#FFFF00',
+      },
+    },
+  },
+  plugins: [],
+}
